cc62ca18312bfb78a1a86fbbc20c83728ba11bab
Norma2026 <normaforyou0@gmail.com>
2026-01-09 23:17:47 +0100

    Adiciona hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+print("Ciao mondo! L'intelligenza artificiale ti saluta!")
+# Presentazione del progetto
+
+Questo è il file README della branch main, che descrive il progetto Progetto1.
+
+---
+
+# Aggiornamento dalla branch New_Branch
+
+Questo è il contenuto aggiunto nella branch New_Branch per completare l’esercizio di merge.
+modifica de prova 
